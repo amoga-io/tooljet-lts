31307e748f7bd2296d9785428a9e1d9a6b7efa45
sharath-amoga <sharath.kumar@amoga.io>
2024-09-18 10:49:00 +0530

    Revert "generate added"
    This reverts commit cbe206eca2b119c6d59b7c72834adbb54cf94904.

--- frontend/src/AppLoader/AppLoader.jsx
+++ frontend/src/AppLoader/AppLoader.jsx
@@ -3,9 +3,9 @@ import { withTranslation } from 'react-i18next';
 import { Editor } from '../Editor/Editor';
 import { RealtimeEditor } from '@/Editor/RealtimeEditor';
 import config from 'config';
-import usePostHog from '../_hooks/usePostHog';
+
 const AppLoaderComponent = React.memo((props) => {
-  usePostHog();
+  console.log('config.ENABLE_MULTIPLAYER_EDITING', config.ENABLE_MULTIPLAYER_EDITING);
   return config.ENABLE_MULTIPLAYER_EDITING ? <RealtimeEditor {...props} /> : <Editor {...props} />;
 });
 
